4bacd515f2c7d06e92c9e39fe43373fa74c38aae
Monther5 <159758834+Monther5@users.noreply.github.com>
2025-09-06 17:15:08 +0200

    Create image_strings.dart

---
+++ lib/core/constants/image_strings.dart
@@ -0,0 +1,11 @@
+class AppImageStrings {
+  // -- SVGs
+  static const String otp = 'assets/images/otp.svg';
+  static const String freshDeals = 'assets/images/fresh_deals.svg';
+
+  // -- PNGs
+  static const String storeFinds = 'assets/images/store_finds.png';
+  static const String headphones = 'assets/images/headphones.png';
+  static const String coffee = 'assets/images/coffee.png';
+  static const String jacket = 'assets/images/jacket.png';
+}
